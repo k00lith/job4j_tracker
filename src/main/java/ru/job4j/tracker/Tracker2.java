e468e7f02eeff9ef1c936be72b81ec768c290506
k00lith <koolith@gmail.com>
2020-12-24 20:58:47 +0300

    2. Singleton. [#413522] устранение дублирования кода

--- src/main/java/ru/job4j/tracker/Tracker2.java
+++ src/main/java/ru/job4j/tracker/Tracker2.java
@@ -1,81 +1,12 @@
 package ru.job4j.tracker;
 
-import java.util.Arrays;
-
 public class Tracker2 {
-    public static Tracker2 instance;
+    public static Tracker instance;
     private Tracker2() {}
-
-    public static Tracker2 getInstance() {
+    public static Tracker getInstance() {
         if (instance == null) {
-            instance = new Tracker2();
+            instance = new Tracker();
         }
         return instance;
     }
-
-    private final Item[] items = new Item[100];
-    private int ids = 1;
-    private int size = 0;
-
-    public Item add(Item item) {
-        item.setId(ids++);
-        items[size++] = item;
-        return item;
-    }
-
-    public Item findById(int id) {
-        /* Находим индекс */
-        int index = indexOf(id);
-        /* Если индекс найден возвращаем item, иначе null */
-        return index != -1 ? items[index] : null;
-    }
-
-    public Item[] findAll() {
-        return Arrays.copyOf(items, size);
-    }
-
-    public Item[] findByName(String key) {
-        Item[] namesSame = new Item[size];
-        int resize = 0;
-        for (int index = 0; index < size; index++) {
-            if (items[index].getName().equals(key)) {
-                namesSame[index] = items[index];
-                resize++;
-            }
-        }
-        return Arrays.copyOf(namesSame, resize);
-    }
-
-    private int indexOf(int id) {
-        int rsl = -1;
-        for (int index = 0; index < size; index++) {
-            if (items[index].getId() == id) {
-                rsl = index;
-                break;
-            }
-        }
-        return rsl;
-    }
-
-    public boolean replace(int id, Item item) {
-        int index = indexOf(id);
-        boolean rsl = index != -1;
-        if (rsl) {
-            item.setId(id);
-            items[index] = item;
-        }
-        return rsl;
-    }
-
-    public boolean delete(int id) {
-        int index = indexOf(id);
-        boolean rsl = index != -1;
-        if (rsl) {
-            System.arraycopy(items, (index + 1), items, index, (size - index));
-            items[size - 1] = null;
-            size--;
-        }
-        return rsl;
-    }
-
 }
