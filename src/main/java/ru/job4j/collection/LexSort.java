e1f6c21e91bb7e07bbaa0c12385a13fb21515826
k00lith <koolith@gmail.com>
2021-01-23 20:38:57 +0300

    4. Сортировка номера [#413460] добавил int leftNumber, int rightNumber

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -8,9 +8,11 @@ public class LexSort implements Comparator<String> {
     public int compare(String left, String right) {
         String[] leftWords = left.split(". ");
         String[] rightWords = right.split(". ");
-        if (Integer.parseInt(leftWords[0]) > Integer.parseInt(rightWords[0])) {
+        int leftNumber = Integer.parseInt(leftWords[0]);
+        int rightNumber = Integer.parseInt(rightWords[0]);
+        if (leftNumber > rightNumber) {
             return 1;
-        } else if (Integer.parseInt(leftWords[0]) < Integer.parseInt(rightWords[0])) {
+        } else if (leftNumber < rightNumber) {
             return -1;
         }
         return 0;
