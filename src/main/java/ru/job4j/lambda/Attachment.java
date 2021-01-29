d6a756cc6855d564339a2a52ddb5b16148c52566
k00lith <koolith@gmail.com>
2021-01-29 20:09:34 +0300

    1. Анонимные классы [#413288]

---
+++ src/main/java/ru/job4j/lambda/Attachment.java
@@ -0,0 +1,19 @@
+package ru.job4j.lambda;
+
+public class Attachment {
+    String name;
+    int size;
+
+    public Attachment(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
