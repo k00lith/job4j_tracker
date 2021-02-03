9da1037719ae486531ee6a9e23c52c5a152d0638
k00lith <koolith@gmail.com>
2021-02-03 14:03:31 +0300

    3. Уникальность элементов и сортировка. [#413300]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -1,5 +1,6 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,8 +13,14 @@ public class Profiles {
      * @return
      */
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream()
+        // формируем список адресов через stream
+        List<Address> addList = profiles.stream()
                 .map(profile -> profile.getAddress())
                 .collect(Collectors.toList());
+        // сортируем по городу
+        addList.sort((Address address1, Address address2)
+                -> address1.getCity().compareTo(address2.getCity()));
+        // возвращаем список адресов без дублей - с помощью distinct
+        return addList.stream().distinct().collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -13,13 +13,19 @@ public class ProfilesTest extends TestCase {
 
     @Test
     public void testCollect() {
-        Address address1 = new Address("Msk", "Street1", 1, 1);
-        Address address2 = new Address("Spb", "Street2", 2, 2);
+        Address address1 = new Address("Aab", "Street1", 1, 1);
+        Address address2 = new Address("Zas", "Street2", 2, 2);
+        Address address3 = new Address("Baa", "Street3", 2, 2);
+        Address address4 = new Address("Kav", "Street2", 2, 2);
+        Address address5 = new Address("Baa", "Street3", 2, 2);
 
         Profile profile1 = new Profile(address1);
         Profile profile2 = new Profile(address2);
-        List<Profile> profileList = Arrays.asList(profile1, profile2);
-        List<Address> addressList = Arrays.asList(address1, address2);
+        Profile profile3 = new Profile(address3);
+        Profile profile4 = new Profile(address4);
+        Profile profile5 = new Profile(address5);
+        List<Profile> profileList = Arrays.asList(profile1, profile2, profile3, profile4, profile5);
+        List<Address> addressList = Arrays.asList(address1, address3, address4, address2);
         assertThat(Profiles.collect(profileList), is(addressList));
     }
 }
