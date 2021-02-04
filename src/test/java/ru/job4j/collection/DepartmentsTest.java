6f0a7fa4f86415af8345c69fe5074d413fa454a2
k00lith <koolith@gmail.com>
2021-02-04 16:17:35 +0300

    1. Метод .of [#413309]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -9,32 +9,32 @@ import static org.junit.Assert.assertThat;
 public class DepartmentsTest {
     @Test
     public void whenMissed() {
-        List<String> input = Arrays.asList("k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenNonChange() {
-        List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenMissedMoreLevels() {
-        List<String> input = Arrays.asList("k1/sk1/ssk2");
-        List<String> expect = Arrays.asList("k1", "k1/sk1", "k1/sk1/ssk2");
+        List<String> input = List.of("k1/sk1/ssk2");
+        List<String> expect = List.of("k1", "k1/sk1", "k1/sk1/ssk2");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void sortAscCodes() {
-        List<String> input = Arrays.asList("K1/SK1/SSK2", "K1", "K1/SK1", "K2/SK1", "K1/SK1/SSK1", "K1/SK2", "K2");
-        List<String> expect = Arrays.asList("K1", "K1/SK1", "K1/SK1/SSK1", "K1/SK1/SSK2", "K1/SK2", "K2", "K2/SK1");
+        List<String> input = List.of("K1/SK1/SSK2", "K1", "K1/SK1", "K2/SK1", "K1/SK1/SSK1", "K1/SK2", "K2");
+        List<String> expect = List.of("K1", "K1/SK1", "K1/SK1/SSK1", "K1/SK1/SSK2", "K1/SK2", "K2", "K2/SK1");
         Departments.sortAsc(input);
         assertThat(input, is(expect));
     }
